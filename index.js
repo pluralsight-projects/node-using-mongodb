7aff235dd4c67944d55a19d3a0de874e7fa87062
mzahaf <mohammed.zahaf@gmail.com>
2021-03-07 12:15:53 +0100

    feat(module2): add mongoose connexion
    Signed-off-by: mzahaf <mohammed.zahaf@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
+// Setting modules
+mongoose.Promise = global.Promise;
+
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Mongoose connection
+mongoose.connection.on('open', () => console.log('Connected to mongo server.'));
+
+mongoose.connection.on('error', (err) => {
+  console.log('Could not connect to mongo server!');
+  return console.log(err);
+});
+
+mongoose.connect('mongodb://localhost/productsdb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongo connected...'));
 
 app.get('/', (req, res) => res.send(`Store server running on port ${PORT}`));
 
